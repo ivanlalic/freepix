ea3e7ed3999eb65dbe420b15f11d0dfca201c919
ivanlalic <ivanlalic@gmail.com>
2020-08-13 01:31:32 -0300

    Updates

---
+++ precache-manifest.0cbb43656540097a180a9208eda5e7c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fbeec22a386de29213075a2ac8e4cf5",
+    "url": "/freepix/index.html"
+  },
+  {
+    "revision": "42988b1ac21c42ebee19",
+    "url": "/freepix/static/js/2.ce1e2c87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/freepix/static/js/2.ce1e2c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6cd5b23ebbfa4108d79",
+    "url": "/freepix/static/js/main.e667fd0a.chunk.js"
+  },
+  {
+    "revision": "94fe2d06127941e8305b",
+    "url": "/freepix/static/js/runtime-main.82aa9f42.js"
+  }
+]);
